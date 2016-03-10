b4329eb4b0ff44293c16bb912ac07ca22e6781b3
Danack <Danack@basereality.com>
2016-03-10 20:15:28 +0000

    Added readme.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+
+Copyright notice
+================
+
+All copyrights retained by the author, but I have no problem if you distribute it with attribution.
+
+
+Run as standalone
+=================
+
+php -S 0.0.0.0:8001 -t public
--- public/index.php
+++ public/index.php
@@ -1,25 +1,27 @@
 <?php
 
-$html = <<< HTML
+require("index.html");
 
-<html>
-<body>
-<p>
-  Were you looking for something?
-</p>
-
-<p>
- Why not try the <a href='/'>index</a> ?
-
-</p>
-
-</body>
-
-
-</html>
-
-
-HTML;
-
-
-echo $html;
+//$html = <<< HTML
+//
+//<html>
+//<body>
+//<p>
+//  Were you looking for something?
+//</p>
+//
+//<p>
+// Why not try the <a href='/index.html'>index</a> ?
+//
+//</p>
+//
+//</body>
+//
+//
+//</html>
+//
+//
+//HTML;
+//
+//
+//echo $html;
