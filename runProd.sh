8e80e97021d8ab3bbc24d61b5c9627033178bc2d
Dan Ackroyd <Danack@basereality.com>
2019-01-06 23:40:45 +0000

    Moved draft talk to given talks.

--- runProd.sh
+++ runProd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose build
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
+docker-compose build --no-cache
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 
 
 
